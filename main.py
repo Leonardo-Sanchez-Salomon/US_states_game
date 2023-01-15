56718206766d8dc68a1916c0720c219c09fc7fbd
Leonardo-Sanchez-Salomon <121777250+Leonardo-Sanchez-Salomon@users.noreply.github.com>
2023-01-15 14:39:44 -0600

    Update main.py
    commented out lines 20-23 and replaced it with more effective list comprehension on line 19

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ states = states_data["state"].to_list()
 while len(correct_guesses) < 50:
     answer_state = screen.textinput(title=f"{guessed}/50 states guessed", prompt="Name another State.").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in correct_guesses:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in correct_guesses]
+#         missing_states = []
+#         for state in states:
+#             if state not in correct_guesses:
+#                 missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
